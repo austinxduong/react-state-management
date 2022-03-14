de179e4a71a267b9e348ee5f3a981554504d8b7f
austinxduong <axduong2@gmail.com>
2022-03-14 15:53:31 -0700

    fixed bug implement try catch API fetch async func

--- src/fetch/Fetch.js
+++ src/fetch/Fetch.js
@@ -12,26 +12,23 @@ import { useState } from "react";
 
 function Fetch() {
 
-    const [data, setData] = useState(["hello"]);
+  const [data, setData] = useState(["hello"]);
 
     const fetchAPI = () => {
-        fetch('https://api.aakhilv.me/fun/wouldyourather')
+        fetch('https://api.aakhilv.me/fun/wyr')
         .then((response) => response.json())
         .then((json) => {
             console.log(json);
-            setData(json.data[0]);
+            setData(json);
     });
-
-}
-
+};
 
   return (
     <div>
-        <button onClick={fetchAPI}>Fetch API</button>
+      <button onClick={fetchAPI}>Fetch API</button>
         <div className='api-card'>
-            <pre className='fetch'>{JSON.stringify(data)}</pre>
+          <p className='fetch'>{JSON.stringify(data)}</p>
         </div>
-        
     </div>
   )
 }
